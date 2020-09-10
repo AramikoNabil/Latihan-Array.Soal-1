227f0fbca330bc4cd654ad58b96f85141f2950a1
sleepy81k <nabilnico@gmail.com>
2020-09-11 03:20:49 +0700

    first

---
+++ Latihan Array.Soal 1.js
@@ -0,0 +1,14 @@
+//Latihan Array
+// *No.1
+// Variabel
+var listDesaBantul =
+  // element array
+  [
+    ["Argodadi", "Argomulyo", "Argorejo", "Argosari"],
+
+    ["Bangunjiwo", "Banguntapan", "Bantul", "Baturetno", "Bawuran"],
+
+    ["Canden", "Caturharjo", "Cetan Srigading"],
+  ];
+// cetak
+console.log(listDesaBantul);
